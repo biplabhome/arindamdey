f5ad32ceca6a96c4b20ce80aecf4d71b8d90d575
Arindam dey <biplabhome@gmail.com>
2019-02-11 12:05:42 +0530

    Update site.js

--- arindamdeyinfo/arindamdeyinfo/wwwroot/js/site.js
+++ arindamdeyinfo/arindamdeyinfo/wwwroot/js/site.js
@@ -1,16 +1,3 @@
 ﻿$(function () {
-    //headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
-    //headers.append('Access-Control-Allow-Credentials', 'true');
-    $.ajax({
-        url: 'https://testwebapibiplabhome.azurewebsites.net/api/values/1',
-        //crossDomain: true,
-        method: 'GET',
-        dataType: 'text'
-    })
-        .done(function (data) {
-            alert(data);
-        })
-        .fail(function (jqXHR, textStatus, err) {
-            alert(jqXHR.statusText);
-        });
+    
 });
