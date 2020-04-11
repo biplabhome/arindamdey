27825f9f3d24e15f658332c2c528d09b6c7f65d6
Arindam Dey <biplabhome@gmail.com>
2020-04-11 12:18:00 +0530

    profileshowcase added

--- src/app/profileshowcase/profileshowcase.component.spec.ts
+++ src/app/profileshowcase/profileshowcase.component.spec.ts
@@ -22,4 +22,5 @@ describe('ProfileshowcaseComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
 });
--- src/app/profileshowcase/profileshowcase.component.ts
+++ src/app/profileshowcase/profileshowcase.component.ts
@@ -11,5 +11,5 @@ export class ProfileshowcaseComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  myPicPath = "assets/img/myPic.png";
 }
