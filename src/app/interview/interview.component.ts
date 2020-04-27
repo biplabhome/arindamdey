0323a6fe2e70881b9baa38916d15125b9ab12952
Arindam Dey <biplabhome@gmail.com>
2020-04-27 20:17:41 +0530

    wip menu structure finalized

--- src/app/interview/interview.component.ts
+++ src/app/interview/interview.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+export interface qs
+{
+  qs: string,
+  ans: string
+}
 @Component({
   selector: 'app-interview',
   templateUrl: './interview.component.html',
   styleUrls: ['./interview.component.css']
 })
+
 export class InterviewComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  qsl: qs[] = [
+    {
+      qs:"Azure Reference Architechture",
+      ans:"https://docs.microsoft.com/en-us/azure/architecture/guide/ \n https://docs.microsoft.com/en-us/azure/architecture/browse/"
+    },
+    {
+      qs:"Azure Services",
+      ans:"https://azure.microsoft.com/en-in/services/"
+    }
+  ];
 }
--- src/app/spiritual/numerology/numerology.component.ts
+++ src/app/spiritual/numerology/numerology.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+export interface numsys{
+  l: string,
+  n: number
+}
 @Component({
   selector: 'app-numerology',
   templateUrl: './numerology.component.html',
@@ -11,5 +14,22 @@ export class NumerologyComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  selectedvalue:string = "";
+  total:number =0;
+  cheroNum: numsys[] = [
+    {l:'a',n:1}, {l:'b',n:1}, {l:'c',n:1}, {l:'d',n:1}, {l:'e',n:1}, {l:'f',n:1}, {l:'g',n:1},
+    {l:'h',n:1}, {l:'i',n:1}, {l:'j',n:1}, {l:'k',n:1}, {l:'l',n:1}, {l:'m',n:1}, {l:'n',n:1},
+    {l:'o',n:1}, {l:'p',n:1}, {l:'q',n:1}, {l:'r',n:1}, {l:'s',n:1}, {l:'t',n:1}, {l:'u',n:1},
+    {l:'v',n:1}, {l:'w',n:1}, {l:'x',n:1}, {l:'y',n:1}, {l:'z',n:1}
+  ];
+  newNum: numsys[] = [
+    {l:'a',n:1}, {l:'b',n:1}, {l:'c',n:1}, {l:'d',n:1}, {l:'e',n:1}, {l:'f',n:1}, {l:'g',n:1},
+    {l:'h',n:1}, {l:'i',n:1}, {l:'j',n:1}, {l:'k',n:1}, {l:'l',n:1}, {l:'m',n:1}, {l:'n',n:1},
+    {l:'o',n:1}, {l:'p',n:1}, {l:'q',n:1}, {l:'r',n:1}, {l:'s',n:1}, {l:'t',n:1}, {l:'u',n:1},
+    {l:'v',n:1}, {l:'w',n:1}, {l:'x',n:1}, {l:'y',n:1}, {l:'z',n:1}
+  ];
+  selectcalculate(boxval:string): void {
+    
+    this.selectedvalue += 1;
+    }
 }
