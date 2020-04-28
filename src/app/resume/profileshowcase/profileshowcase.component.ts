a4c608b95a4dc969c16f1155a2080ba3a7abfc79
Arindam Dey <biplabhome@gmail.com>
2020-04-28 13:12:36 +0530

    profile set

--- src/app/resume/profileshowcase/profileshowcase.component.ts
+++ src/app/resume/profileshowcase/profileshowcase.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {TooltipPosition} from '@angular/material/tooltip';
+import {DomSanitizer} from '@angular/platform-browser';
+import {MatIconRegistry} from '@angular/material/icon';
 
 @Component({
   selector: 'app-profileshowcase',
@@ -9,16 +11,24 @@ import {TooltipPosition} from '@angular/material/tooltip';
 })
 export class ProfileshowcaseComponent implements OnInit {
 
-  constructor() { }
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) { 
+    iconRegistry.addSvgIcon(
+      'mobile',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/menu/call-24px.svg'))
+      .addSvgIcon(
+        'email',
+        sanitizer.bypassSecurityTrustResourceUrl('assets/img/menu/email-24px.svg'))
+  }
 
   ngOnInit(): void {
   }
   myPicPath = "assets/img/myPic.png";
-  facebookPicPath = "assets/img/facebook.png";
-  linkedinPicPath = "assets/img/linkedin.png";
-  twitterPicPath = "assets/img/twitter.png";
-  hangoutsPicPath = "assets/img/hangouts.png";
-  skypePicPath = "assets/img/skype.png";
+  facebookPicPath = "assets/img/socialicons/facebook.png";
+  linkedinPicPath = "assets/img/socialicons/linkedin.png";
+  twitterPicPath = "assets/img/socialicons/twitter.png";
+  hangoutsPicPath = "assets/img/socialicons/hangouts.png";
+  skypePicPath = "assets/img/socialicons/skype.png";
+  gravatarPicPath = "assets/img/socialicons/gravatar.png";
 
   positionOptions: TooltipPosition[] = ['after', 'before', 'above', 'below', 'left', 'right'];
   position = new FormControl(this.positionOptions[0]);
